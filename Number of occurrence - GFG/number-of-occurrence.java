537b19675244aa89788c565a8ce85adcfd9c7926
Dhananjoy Maji <102242946+DhananjoyMajiIT@users.noreply.github.com>
2023-08-20 10:50:28 +0530

    Added solution - LeetHub

--- Number of occurrence - GFG/number-of-occurrence.java
+++ Number of occurrence - GFG/number-of-occurrence.java
@@ -51,26 +51,64 @@ class Solution {
         
         //the question tell me solve the problem  T(C)=O(log N)
         
-         int count=0;
-         int i=0;
+        int left_idx=binaryL(arr,0,n-1,x);
+        int right_idx=binaryR(arr,0,n-1,x);
+        
+        
+        return right_idx-left_idx;
+        
+        //======================================================================
+        
+        //  int count=0;
+        //  int i=0;
          
-         if(arr[0]>x)return 0;
-         if(arr[n-1]<x)return 0;
+        //  if(arr[0]>x)return 0;
+        //  if(arr[n-1]<x)return 0;
          
-         while(i<n){
-             if(arr[i]<x){
-                 while(arr[i]==arr[i+1]){
-                     i++;
-                 }
-             }
+        //  while(i<n){
+        //      if(arr[i]<x){
+        //          while(arr[i]==arr[i+1]){
+        //              i++;
+        //          }
+        //      }
              
-             if(arr[i]==x){
-                 count++;
-             }
+        //      if(arr[i]==x){
+        //          count++;
+        //      }
              
-             if(arr[i]>x)break;
-             i++;
-         }
-         return count;
+        //      if(arr[i]>x)break;
+        //      i++;
+        //  }
+        //  return count;
+    }
+    
+    static int binaryL(int []arr,int st,int end,int x){
+        while(st<=end){
+            int mid=(st+end)/2;
+            
+            if(arr[mid]>=x){
+                end=mid-1;
+            }
+            else{
+                st=mid+1;
+            }
+        }
+       
+        return end;
+    }
+    
+    static int binaryR(int []arr,int st,int end,int x){
+        while(st<=end){
+            int mid=(st+end)/2;
+            
+            if(arr[mid]<=x){
+                st=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+        }
+        
+        return end;
     }
 }
