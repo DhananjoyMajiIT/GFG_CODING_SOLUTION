9103014b8ec1ca8f78e6f3fe5fa4200333614d06
Dhananjoy Maji <102242946+DhananjoyMajiIT@users.noreply.github.com>
2023-09-26 17:25:36 +0530

    Added solution - LeetHub

---
+++ Find All Four Sum Numbers - GFG/find-all-four-sum-numbers.java
@@ -0,0 +1,85 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+
+        while (tc-- > 0) {
+            String[] nk = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(nk[0]);
+            int k = Integer.parseInt(nk[1]);
+
+            String[] str = br.readLine().trim().split(" ");
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(str[i]);
+            }
+
+            Solution sln = new Solution();
+            ArrayList<ArrayList<Integer>> ans = sln.fourSum(a, k);
+            for (ArrayList<Integer> v : ans) {
+                for (int u : v) {
+                    System.out.print(u + " ");
+                }
+                System.out.print("$");
+            }
+            if (ans.isEmpty()) {
+                System.out.print(-1);
+            }
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+
+
+// User function Template for Java
+
+// arr[] : int input array of integers
+// k : the quadruple sum required
+
+class Solution {
+    public ArrayList<ArrayList<Integer>> fourSum(int[] nums, int target) {
+        // code here
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+        int n = nums.length;
+        Arrays.sort(nums);
+
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            for (int j = i + 1; j < n; j++) {
+                if (j != i + 1 && nums[j] == nums[j - 1]) continue;
+                int l = j + 1, h = n - 1;
+                while (l < h) {
+                    int sum =  nums[i] + nums[j] + nums[l] + nums[h];
+                    if (sum < target) {
+                        l++;
+                    } else if (sum > target) {
+                        h--;
+                    } else {
+                        ArrayList<Integer>temp=new ArrayList<>();
+                        temp.add(nums[i]);
+                        temp.add(nums[j]);
+                        temp.add(nums[l]);
+                        temp.add(nums[h]);
+                       // ans.add(Arrays.asList(nums[i], nums[j], nums[l], nums[h]));
+                        ans.add(temp);
+                        l++;
+                        h--;
+                        while (l < h && nums[l] == nums[l - 1]) l++;
+                        while (l < h && nums[h] == nums[h + 1]) h--;
+                    }
+                }
+            }
+        }
+    return ans;
+    }
+}
