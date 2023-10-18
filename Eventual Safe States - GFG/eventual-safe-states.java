9d4293612b3628741ddb6dcea1488f38cba2f13c
Dhananjoy Maji <102242946+DhananjoyMajiIT@users.noreply.github.com>
2023-10-18 19:14:50 +0530

    Added solution - LeetHub

---
+++ Eventual Safe States - GFG/eventual-safe-states.java
@@ -0,0 +1,76 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// Position this line where user code will be pasted.
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int V = sc.nextInt();
+            int E = sc.nextInt();
+
+            List<List<Integer>> adj = new ArrayList<>();
+
+            for (int i = 0; i < V; i++) {
+                adj.add(new ArrayList<>());
+            }
+            for (int i = 0; i < E; i++) {
+                int u = sc.nextInt();
+                int v = sc.nextInt();
+
+                adj.get(u).add(v);
+            }
+
+            Solution obj = new Solution();
+            List<Integer> safeNodes = obj.eventualSafeNodes(V, adj);
+            for (int i : safeNodes) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+
+    List<Integer> eventualSafeNodes(int V, List<List<Integer>> adj) {
+        
+        List<Integer>safeNode=new ArrayList<Integer>();
+        
+        int[]visit=new int[V];
+        for(int i=0;i<V;i++){
+            if(IsSafe(i,adj,visit)){
+                safeNode.add(i);
+            }
+        }
+        Collections.sort(safeNode);
+        return safeNode;
+    }
+    static boolean IsSafe(int idx, List<List<Integer>> adj,int[]visit){
+        
+        if(visit[idx]!=0){
+            return visit[idx]==2;
+        }
+        visit[idx]=1;
+        
+        for(int nei:adj.get(idx)){
+             if(!IsSafe(nei,adj,visit)){
+                 return false;
+             }
+        }
+        
+        visit[idx]=2;
+        return true;
+        
+    }
+}
