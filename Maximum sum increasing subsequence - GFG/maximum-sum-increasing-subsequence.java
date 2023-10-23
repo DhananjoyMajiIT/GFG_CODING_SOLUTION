fd7f5436d4c41d56a327913e17dfe3d9b080c800
Dhananjoy Maji <102242946+DhananjoyMajiIT@users.noreply.github.com>
2023-10-23 12:19:35 +0530

    Added solution - LeetHub

---
+++ Maximum sum increasing subsequence - GFG/maximum-sum-increasing-subsequence.java
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main(String args[])
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    int n = sc.nextInt();
+                    int Arr[] = new int[n];
+                    for(int i = 0;i<n;i++)
+                        Arr[i] = sc.nextInt();
+                    Solution ob = new Solution();
+                    System.out.println(ob.maxSumIS(Arr,n));
+                }
+        }
+}    
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+	public int maxSumIS(int arr[], int n)  
+	{  
+	    //code here.
+	    int dp[]=new int[n];
+	    int maxsum=0;
+	    for(int i=0;i<n;i++){
+	        dp[i]=arr[i];
+	    }
+	    
+	    for(int i=0;i<n;i++){
+	        for(int j=0;j<i;j++){
+	            if(arr[i]>arr[j]){
+	                dp[i]=Math.max(dp[i],dp[j]+arr[i]);
+	            }
+	        }
+	        maxsum=Math.max(dp[i],maxsum);
+	    }
+	    return maxsum;
+	}  
+}
