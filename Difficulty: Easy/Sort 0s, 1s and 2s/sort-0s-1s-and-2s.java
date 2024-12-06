b02a91dfca0108b149d42846b5f4a57cd368ee37
Dhananjoy Maji <102242946+DhananjoyMajiIT@users.noreply.github.com>
2024-12-06 11:21:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.java
+++ Difficulty: Easy/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.java
@@ -11,11 +11,9 @@ class GFG {
         while (t-- > 0) {
             String input = br.readLine();
             String[] inputArray = input.split("\\s+");
-            ArrayList<Integer> a = new ArrayList<>();
+            int a[] = new int[inputArray.length];
 
-            for (String s : inputArray) {
-                a.add(Integer.parseInt(s));
-            }
+            for (int i = 0; i < a.length; i++) a[i] = Integer.parseInt(inputArray[i]);
 
             Solution ob = new Solution();
             ob.sort012(a);
@@ -24,41 +22,43 @@ class GFG {
                 System.out.print(num + " ");
             }
             System.out.println();
+            System.out.println("~");
         }
     }
 }
 
+
 // } Driver Code Ends
-
-
 class Solution {
     // Function to sort an array of 0s, 1s, and 2s
-    public void sort012(ArrayList<Integer> arr) {
+    public void sort012(int[] arr) {
         // code here
-        int zero=0;
-        int one=0;
-        int two=0;
-        for(int i=0;i<arr.size();i++){
-            if(arr.get(i)==0){
+        int zero=0,one=0,two=0;
+        
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==0){
                 zero++;
-            }else if(arr.get(i)==1){
+            }else if(arr[i]==1){
                 one++;
             }else{
                 two++;
             }
         }
-        int idx=0;
-        while(zero-->0){
-            arr.set(idx,0);
-            idx++;
+        int j=0;
+        while(zero!=0){
+            arr[j++]=0;
+            zero--;
         }
-        while(one-->0){
-            arr.set(idx,1);
-            idx++;
+        while(one!=0){
+            arr[j++]=1;
+            one--;
         }
-        while(two-->0){
-            arr.set(idx,2);
-            idx++;
+        while(two!=0){
+            arr[j++]=2;
+            two--;
         }
     }
 }
+
+//{ Driver Code Starts.
+// } Driver Code Ends
