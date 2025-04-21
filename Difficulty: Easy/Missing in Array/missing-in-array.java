4d783b9829a29e9bf00d8395799fd60b3db399ed
Dhananjoy Maji <102242946+DhananjoyMajiIT@users.noreply.github.com>
2025-04-21 20:22:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Missing in Array/missing-in-array.java
+++ Difficulty: Easy/Missing in Array/missing-in-array.java
@@ -1,45 +1,32 @@
 //{ Driver Code Starts
-// Initial Template for Java
-
 import java.io.*;
 import java.util.*;
 
-class GFG {
-
-    public static void main(String[] args) throws IOException {
+class Geeks {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int t = Integer.parseInt(br.readLine().trim());
-        while (t-- > 0) {
-            int n = Integer.parseInt(br.readLine().trim());
-            String[] str = br.readLine().trim().split(" ");
-            int[] arr = new int[n - 1];
-            for (int i = 0; i < n - 1; i++) {
+        int t = Integer.parseInt(br.readLine());
+        for (int g = 0; g < t; g++) {
+            String[] str = (br.readLine()).trim().split(" ");
+            int arr[] = new int[str.length];
+            for (int i = 0; i < str.length; i++) {
                 arr[i] = Integer.parseInt(str[i]);
             }
-            Solution sln = new Solution();
-            System.out.println(sln.missingNumber(n, arr));
+            System.out.println(new Solution().missingNum(arr));
+            System.out.println("~");
         }
     }
 }
 // } Driver Code Ends
 
 
-// User function Template for Java
 class Solution {
-
-    // Note that the size of the array is n-1
-    int missingNumber(int n, int arr[]) {
-        int sum1=0;
-        int sum=0;
-        int num=-1;
-        for(int i=0;i<arr.length;i++){
-            sum1 +=arr[i];
-        }
-        for(int i=1;i<=n;i++){
-            sum +=i;
-        }
-         num=sum-sum1;
-        return num;
+    int missingNum(int arr[]) {
+        // code here
+     
+        int xorr = 0;
+        for(int i = 1 ; i <= arr.length + 1 ; i++) xorr ^= i;
+        for(int val : arr) xorr ^= val;
+        return xorr;
     }
 }
